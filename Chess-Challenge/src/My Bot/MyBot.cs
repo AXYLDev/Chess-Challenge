d00feb9e749b01c69f33a4713039f80a3f362944
axyl9@icloud.com <axyl9@icloud.com>
2023-07-22 17:13:44 +0100

    First iteration of AI.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,9 +2,32 @@
 
 public class MyBot : IChessBot
 {
+    uint s_maxDepth = 3;
+    float[] s_pieceValues = { 0, 1, 3, 3, 5, 9 };
+    bool m_isWhite;
+
     public Move Think(Board board, Timer timer)
     {
+        m_isWhite = board.IsWhiteToMove;
+        float max = -1024f;
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move bestMove = moves[0];
+        for (uint i = 0; i < moves.Length; i++) { 
+            float v = DeltaValue(moves[i], board, 0);
+            if (v > max) { max = v; bestMove = moves[i]; }
+        }
+        return bestMove;
+    }
+
+    float DeltaValue(Move move, Board board, uint depth)
+    {
+        float value = (m_isWhite == board.IsWhiteToMove ? 1 : -1) * s_pieceValues[(int)board.GetPiece(move.TargetSquare).PieceType];
+        if (depth >= s_maxDepth) return value;
+        board.MakeMove(move);
+        Move[] moves = board.GetLegalMoves();
+        for (uint i = 0; i < moves.Length; i++)
+            value += DeltaValue(moves[i], board, depth + 1) / moves.Length;
+        board.UndoMove(move);
+        return value;
     }
 }
